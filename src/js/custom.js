561f83916a9301a3cd52f5af24b673c7d6805474
Soar Lin <Soar.Lin@ruckuswireless.com>
2023-12-20 01:20:34 +0800

    adjust theme color

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,31 @@
-$('#myCarousel').on('click', () => {
-  console.log('jQuery is working');
+// $('#myCarousel').on('click', () => {
+//   console.log('jQuery is working');
+// });
+
+// THEME
+$("#themeBtn").on("click", function() {
+  toggleTheme();
 });
+
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+if (localStorage.getItem('theme') === 'theme-dark'){
+    setTheme('theme-light');
+} else {
+    setTheme('theme-dark');
+}
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark');
+} else {
+    setTheme('theme-light');
+}
+})();
