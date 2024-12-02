12f0eafd84e7eb9f3fcebde934bcda7c0b904916
Tonya Li <a78b12c26@gmail.com>
2024-12-02 22:29:26 +0800

    add svg close & hover ani

--- src/js/_menu.js
+++ src/js/_menu.js
@@ -3,21 +3,25 @@
 (function () {
   document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.querySelector('#menuCloseBtn');
+    const menuIcon = document.querySelector('.menu-icon');
     const menu = document.querySelector('#menu');
+
 
     const toggleMenu = (action) => {
       if (action === 'open') {
+        menuIcon.classList.add('menu-open');
         menu.classList.add('menu--opening');
         setTimeout(() => {
           menu.classList.remove('menu--opening');
           menu.classList.add('menu--show');
-        }, 1000); // 1秒動畫結束
+        }, 700); // 動畫結束
       } else if (action === 'close') {
+        menuIcon.classList.remove('menu-open');
         menu.classList.add('menu--closing');
         setTimeout(() => {
           menu.classList.remove('menu--closing');
           menu.classList.remove('menu--show');
-        }, 1000); // 1秒動畫結束
+        }, 700); // 動畫結束
       }
     };
 
