8f61d2c8b9b0077eb05042bcfe00122578c01849
Tonya Li <a78b12c26@gmail.com>
2024-12-02 22:52:30 +0800

    fix menu

--- src/js/_menu.js
+++ src/js/_menu.js
@@ -5,27 +5,32 @@
     const menuButton = document.querySelector('#menuCloseBtn');
     const menuIcon = document.querySelector('.menu-icon');
     const menu = document.querySelector('#menu');
-
+    let isAnimation = false
 
     const toggleMenu = (action) => {
       if (action === 'open') {
+        isAnimation = true
         menuIcon.classList.add('menu-open');
         menu.classList.add('menu--opening');
         setTimeout(() => {
           menu.classList.remove('menu--opening');
           menu.classList.add('menu--show');
-        }, 700); // 動畫結束
+          isAnimation = false
+        }, 300); // 動畫結束
       } else if (action === 'close') {
+        isAnimation = true
         menuIcon.classList.remove('menu-open');
         menu.classList.add('menu--closing');
         setTimeout(() => {
           menu.classList.remove('menu--closing');
           menu.classList.remove('menu--show');
-        }, 700); // 動畫結束
+          isAnimation = false
+        }, 300); // 動畫結束
       }
     };
 
     menuButton.addEventListener('click', () => {
+      if (isAnimation) return;
       if (!menu.classList.contains('menu--show')) {
         toggleMenu('open');
       } else {
